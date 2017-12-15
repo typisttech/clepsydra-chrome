1e393802518e1837a1158a6e318966fbe120132e
Tang Rufus <tangrufus@gmail.com>
2017-12-16 01:11:24 +0800

    yarn run lint:fix

--- src/Tabs.js
+++ src/Tabs.js
@@ -1,7 +1,5 @@
 import BrowserTabs from './browser/BrowserTabs';
 
 export default class Tabs {
-  static open = async (url) => {
-    return BrowserTabs.open(url);
-  }
+  static open = async url => BrowserTabs.open(url)
 }
